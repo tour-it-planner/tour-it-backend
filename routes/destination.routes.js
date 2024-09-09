96d0464f441d668e015cd7afa24d7058ca57a107
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-09 15:23:12 +0100

    add promise for itineraries
    Co-authored-by: RutuPalwekar <RutuPalwekar@users.noreply.github.com>

--- routes/destination.routes.js
+++ routes/destination.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+
 const Destination = require("../models/Destination.model");
+const Itinerary = require("../models/Itinerary.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 //Post /api/destinations - creates new location
@@ -23,15 +25,24 @@ router.get("/destinations", (req, res, next) => {
 
 // GET /api/destinations/:destinationId
 
-router.get("/destinations/:destinationId", (req, res, next) => {
+router.get("/destinations/:destinationId", isAuthenticated, (req, res, next) => {
     const { destinationId } = req.params;
+    let destinationDetails;
 
     if (!mongoose.Types.ObjectId.isValid(destinationId)) {
         res.status(400).json({ message: "Specified id is not valid" });
         return;
     }
     Destination.findById(destinationId)
-        .then((destination) => res.status(200).json(destination))
+    .then( destinationFromDB =>{
+        destinationDetails=destinationFromDB;
+       return Itinerary.find({destinations: destinationId})
+    })
+        .then( itinerariesFromDB => {
+            destinationDetails = destinationDetails.toObject(); 
+            destinationDetails.itineraries = itinerariesFromDB;
+            res.status(200).json(destinationDetails);
+        })
         .catch((error) => res.json(error));
 });
 
