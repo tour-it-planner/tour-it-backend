4010600cfa9436445e8ad37e9585a05c15f31fb4
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-09-13 09:10:12 +0100

    add healthcheck endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,14 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+
+router.get('/health', (req, res) => {
+  res.status(200).json({
+      status: "OK",
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString()
+  });
+});
+
+
 module.exports = router;
